e865a4fc90fcc51629c9fd2027a17f7d31ec514c
Usman Akram <uakram86@gmail.com>
2022-05-25 16:51:05 +0100

    managed to link restaurants to reviews - code still not great though

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
 
   validates :content, presence: true
   validates :restaurant, presence: true
-  validates :rating, presence: true, inclusion: { in: 0..5 }, numericality: { only_integer: true}
+  validates :rating, presence: true, inclusion: { in: 0..5 }, numericality: { only_integer: true }
 end
